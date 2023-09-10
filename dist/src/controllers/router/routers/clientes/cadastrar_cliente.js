296589eab1f25a5fb1a99fa3cdee2acad08f40b0
Fabricio <banksapk@gmail.com>
2023-09-10 15:07:14 -0300

    cadastrar cliente

--- dist/src/controllers/router/routers/clientes/cadastrar_cliente.js
+++ dist/src/controllers/router/routers/clientes/cadastrar_cliente.js
@@ -1,11 +1,35 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.cadastrarCliente = void 0;
+const cliente_1 = require("../../../../modules/domain/cliente/cliente");
+const consulta_1 = require("../../../../modules/shared/consulta");
+const dentista_1 = require("../../../../modules/domain/dentista/dentista");
 class CadastrarCliente {
     cadastro(req, res) {
         try {
             let { nome, idade, CPF, CPFresponsavel, telefone } = req.body;
-            console.log(nome);
+            let { nomeDentista, CFOID, cirurgiao } = req.body;
+            let dentista = dentista_1.Dentista.createDentista({
+                nomeDentista,
+                CFOID,
+                cirurgiao,
+            });
+            let { dia, mes, ano } = req.body;
+            let consulta = [
+                new consulta_1.Consulta(new Date(ano - dia - mes), dentista),
+            ];
+            const cliente = cliente_1.Cliente.createCliente({
+                nome,
+                idade,
+                CPF,
+                CPFresponsavel,
+                consulta,
+                telefone,
+            });
+            console.log(cliente);
+            res.json({
+                dados: cliente,
+            });
         }
         catch (e) {
             console.log(`There was an error${e}`);
--- src/controllers/router/routers/clientes/cadastrar_cliente.ts
+++ src/controllers/router/routers/clientes/cadastrar_cliente.ts
@@ -14,7 +14,31 @@ import {
 class CadastrarCliente {
   public cadastro(req: Request, res: Response) {
     try {
-      let { nome, idade, CPF, CPFresponsavel, consultas, telefone } = req.body;
+      let { nome, idade, CPF, CPFresponsavel, telefone } = req.body;
+      let { nomeDentista, CFOID, cirurgiao }: IDentista = req.body;
+
+      let dentista: Dentista = Dentista.createDentista({
+        nomeDentista,
+        CFOID,
+        cirurgiao,
+      });
+      let { dia, mes, ano } = req.body;
+      let consulta: Consulta[] = [
+        new Consulta(new Date(ano - dia - mes), dentista),
+      ];
+
+      const cliente = Cliente.createCliente({
+        nome,
+        idade,
+        CPF,
+        CPFresponsavel,
+        consulta,
+        telefone,
+      });
+      console.log(cliente);
+      res.json({
+        dados: cliente,
+      });
     } catch (e) {
       console.log(`There was an error${e}`);
     }
