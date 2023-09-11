9d5665813535db48e37791b60440537af26a9c3d
Fabricio <banksapk@gmail.com>
2023-09-11 14:04:13 -0300

    rota cadastrar dentista entity

--- src/controllers/router/router.ts
+++ src/controllers/router/router.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { homePage } from "./routers/homepage/homepage";
 import { cadastrarCliente } from "./routers/clientes/cadastrar_cliente";
+import { cadastrarDentista } from "./routers/dentistas/cadastrar.dentista";
 
 export const router: Router = Router();
 
 router.get("/", homePage.home);
 router.post("/cadastrar/cliente", cadastrarCliente.cadastro);
+router.post("/cadastrar/dentista", cadastrarDentista.cadastrarDentista);
---
+++ src/controllers/router/routers/dentistas/cadastrar.dentista.ts
@@ -0,0 +1,25 @@
+import { Dentista } from "../../../../modules/domain/dentista/dentista";
+import { IDentista } from "../../../../modules/domain/dentista/iDentista";
+import { DentistaDTO } from "../../../../modules/mappers/dentista.map";
+import { Request, Response } from "express";
+
+export class CadastrarDentista {
+  public cadastrarDentista(req: Request, res: Response) {
+    let { nomeDentista, CFOID, cirurgiao }: IDentista = req.body;
+
+    try {
+      const dentista = Dentista.createDentista({
+        nomeDentista,
+        CFOID,
+        cirurgiao,
+      });
+      res.json({
+        dados_dentista: DentistaDTO.dentistaDTO(dentista),
+      });
+    } catch (e) {
+      console.log(`There was an error! ${e}`);
+    }
+  }
+}
+
+export const cadastrarDentista = new CadastrarDentista();
